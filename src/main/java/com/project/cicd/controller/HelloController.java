50e2d28481e23502a40ebce6b8494b420d0cbcdd
youngjun <sohn919@naver.com>
2023-08-01 19:53:04 +0900

    init project

---
+++ src/main/java/com/project/cicd/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.project.cicd.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "hello-ec2";
+    }
+}
